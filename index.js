17580fd9585ea6016cca72f263a851a37869d54e
Natalia Davtyan <110120210+nataliadavtyan@users.noreply.github.com>
2023-01-25 12:39:45 +0100

    Made code dryer

--- index.js
+++ index.js
@@ -6,56 +6,40 @@ const profileContainer = document.getElementById('profile-container')
 const badgeLike = document.getElementById('badge-like')
 const badgeNope = document.getElementById('badge-nope')
 
-document.getElementById("swipe-left").addEventListener("click", nope)
-document.getElementById("swipe-right").addEventListener('click', like)
+document.getElementById("swipe-left").addEventListener("click", function() {
+    displayMatchStatus(badgeNope)})
+document.getElementById("swipe-right").addEventListener('click', function() {
+    displayMatchStatus(badgeLike)})
+    
+let isWaiting = false
+
+function displayMatchStatus(badge){
+    if (!isWaiting) {
+        if (badge === badgeLike) {
+            currentDog.setMatchStatus(true)
+        } 
+        badge.classList.toggle('hidden')
+        isWaiting = true
+        
+        setTimeout(() => {
+            badge.classList.toggle('hidden')
+            isWaiting = false
+            
+            if (dogsData.length > 0) {
+                currentDog = getNewDog()
+                render()
+            }
+            else {
+                end()
+            }
+        }, 1000)
+    }
+}
 
 function getNewDog(){
     const nextDogData = dogsData.shift()
     return nextDogData ? new Dog(nextDogData) : {}
 }
-
-let isWaiting = false
-
-function like() {
-    if (!isWaiting) {
-        currentDog.setMatchStatus(true)
-        badgeLike.classList.toggle('hidden')
-        isWaiting = true
-        
-        setTimeout(() => {
-            badgeLike.classList.toggle('hidden')
-            isWaiting = false
-            
-            if (dogsData.length > 0) {
-                currentDog = getNewDog()
-                render()
-            }
-            else {
-                end()
-            }
-        }, 1000)
-    }
-}
-
-function nope() {
-    if (!isWaiting) {
-        badgeNope.classList.toggle('hidden')
-        isWaiting = true
-        
-        setTimeout(() => {
-            badgeNope.classList.toggle('hidden')
-            isWaiting = false
-            
-            if (dogsData.length > 0) {
-                currentDog = getNewDog()
-                render()
-            }
-            else {
-                end()
-            }
-        }, 1000)
-    }
-}       
 
 function end() {
     profileContainer.innerHTML = `
